6ff8cc7df06a0991fd62ca8b41ec63c2cf55dfb9
yiwiy9 <yiwiy9@gmail.com>
2022-04-05 20:33:59 +0900

    Fix get.py and delete.py

--- src/functions/delete.py
+++ src/functions/delete.py
@@ -1,6 +1,23 @@
-def delete() -> dict:
-    return {'statusCode': 200, 'body': 'Deleted!'}
+import http.client as httplib
+
+from pynamodb.exceptions import DoesNotExist, DeleteError
+from src.models import CompareYourselfModel
 
 
 def handler(event, context):
-    return delete()
+    print('event: {}'.format(event))
+    user_id = 'b80199e5-b445-11ec-9fa4-61a43020f107' # 仮
+
+    try:
+        model = CompareYourselfModel.get(hash_key=user_id)
+    except DoesNotExist:
+        return {'statusCode': httplib.NOT_FOUND,
+                'body': 'CompareYoreself {} not found'.format(user_id)}
+
+    try:
+        model.delete()
+    except DeleteError:
+        return {'statusCode': httplib.BAD_REQUEST,
+                'body': 'Unable to delete CompareYoreself {}'.format(user_id)}
+
+    return {'statusCode': httplib.NO_CONTENT}
--- src/functions/get.py
+++ src/functions/get.py
@@ -1,11 +1,31 @@
-def get(type: str) -> dict:
-    if (type == 'all'):
-        return {'statusCode': 200, 'body': 'All data is here!'}
-    elif (type == 'single'):
-        return {'statusCode': 200, 'body': 'The single user data is here!'}
-    else:
-        return {'statusCode': 200, 'body': 'Hello from lambda'}
+import json
+import http.client as httplib
+
+from pynamodb.exceptions import DoesNotExist
+from src.models import CompareYourselfModel
 
 
 def handler(event, context):
-    return get(type=event['pathParameters']['type'])
+    print('event: {}'.format(event))
+    type = event['pathParameters']['type']
+
+    if (type == 'all'):
+        models = CompareYourselfModel.scan()
+        return {'statusCode': httplib.OK,
+                'body': json.dumps([dict(model) for model in models])}
+
+    elif (type == 'single'):
+        user_id = 'b80199e5-b445-11ec-9fa4-61a43020f107' # 仮
+
+        try:
+            model = CompareYourselfModel.get(hash_key=user_id)
+        except DoesNotExist:
+            return {'statusCode': httplib.NOT_FOUND,
+                    'body': 'CompareYoreself {} not found'.format(user_id)}
+
+        return {'statusCode': httplib.OK,
+                'body': json.dumps([dict(model)])}
+
+    else:
+        return {'statusCode': httplib.BAD_REQUEST,
+                'body': 'Invalid Path Parameter was given'}
